60b249d2ca30c15fe5cc336bfc69256034807b96
Dan <danb7.web@gmail.com>
2024-01-04 17:24:23 -0700

    Created threading.excepthook method to handle unexpected errors during download in main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from tkinter.messagebox import showerror
+
+import threading
+
 from PyTubeAppClass import *
 
 # Driver file to serve as entry point, creates and executes instance of YouTubeDownloaderApp
+
+# Substitute method as threading.excepthook
+# Designed to handle unexpected exceptions in download thread by displaying exception value in message box
+def unexpectedError(args):
+    showerror('Error', args.exc_value)
 
 # Main method creates instance of YouTubeDownloaderApp "app" and runs it
 def main() -> None:
@@ -8,5 +17,7 @@ def main() -> None:
     app.run()
 
 # Call main() method to construct and display GUI from PyTubeAppClass.py if run as script
+# threading.excepthook specified
 if __name__ == "__main__":
+    threading.excepthook = unexpectedError
     main()
